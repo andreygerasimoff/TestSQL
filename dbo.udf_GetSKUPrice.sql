4a1983bda8363d621b34ac658573ac878f645a1e
andreygerasimoff <72318091+andreygerasimoff@users.noreply.github.com>
2023-04-01 18:18:27 +0300

    Update dbo.udf_GetSKUPrice.sql

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -2,9 +2,9 @@ create function udf_GetSKUPrice
  (@ID_SKU int)
 returns decimal(18, 2)
  begin
-  declare @Summ decimal(18, 2)
-  select @Summ = ((Value + @ID_SKU)/(Quantity + @ID_SKU))
+  select @ID_SKU = (sum(Value)/sum(Quantity))
   from dbo.Basket
   where ID_SKU=@ID_SKU
-  return @Summ
+  return @ID_SKU
  end
+
